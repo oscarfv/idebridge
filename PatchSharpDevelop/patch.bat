b977ca0628b8ad607b55de272c73eeab5da5886f
rdx.b2c@free.fr <rdx.b2c@free.fr@31b512bc-d2bf-11de-97ae-379497272b75>
2009-11-17 16:04:56 +0000

    git-svn-id: http://idebridge.googlecode.com/svn/trunk@14 31b512bc-d2bf-11de-97ae-379497272b75

---
+++ PatchSharpDevelop/patch.bat
@@ -0,0 +1,3 @@
+copy AbstractCompletionWindow.cs %1%\src\Libraries\ICSharpCode.TextEditor\Project\Src\Gui\CompletionWindow
+copy CodeCompletionWindow.cs %1%\src\Libraries\ICSharpCode.TextEditor\Project\Src\Gui\CompletionWindow
+copy SharpDevelopTextAreaControl.cs %1%\src\Main\Base\Project\Src\TextEditor\Gui\Editor
