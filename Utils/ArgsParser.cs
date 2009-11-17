5f7d9525974adf801e5e24c19905d541effc1990
rdx.b2c@free.fr <rdx.b2c@free.fr@31b512bc-d2bf-11de-97ae-379497272b75>
2009-11-17 15:25:57 +0000

    git-svn-id: http://idebridge.googlecode.com/svn/trunk@9 31b512bc-d2bf-11de-97ae-379497272b75

---
+++ Utils/ArgsParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdeBridge
+{
+    public class ArgsParser
+    {
+        Char [] _sep = new Char[] { ',' };
+        int _argsIndex = 0;
+        string[] _args = null;
+        
+        public void Init(object input) 
+        {
+            _args = (input as string).Split(_sep, StringSplitOptions.RemoveEmptyEntries);
+            _argsIndex = 0;
+        }
+
+        public IntPtr[] ReadIntPtrArray()  
+        {
+            IntPtr[] result = new IntPtr[_args.Length];
+            for(int i =0; i < _args.Length; i++)  
+            {
+                result[i] = ReadIntPtr(_args[i]);
+            }
+            return result;
+        }
+
+        public IntPtr ReadIntPtr()  
+        {
+            return ReadIntPtr(_args[_argsIndex++]);
+        }
+
+        public IntPtr ReadIntPtr(string value)  
+        {
+            if( IntPtr.Size == 8 ) 
+            {
+                return new IntPtr(Int32.Parse(value));
+            }
+            else
+            {
+                return new IntPtr(Int64.Parse(value));
+            }
+        }
+
+        public int ReadInt()  
+        {
+            return Int32.Parse(_args[_argsIndex++]);
+        }
+    }
+}
---
+++ Utils/Logger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+//using EnvDTE;
+
+namespace IdeBridge
+{
+    public class Logger
+    {
+        public static RichTextBox Output = null;
+        //public static OutputWindowPane OutputPane = null;
+ 
+        public static void Error(string message)
+        {
+            Info(message);
+//             if (OutputPane != null) 
+//             {
+//                 OutputPane.OutputString(message);                
+//             }
+//             else
+//             {
+
+//                 MessageBox.Show(message);
+//             }
+        }
+
+        public static void Debug(string message)
+        {
+            Info(message);
+        }
+        
+        public static void Exception(Exception e) 
+        {
+            string message = "";
+            if (e != null) 
+            {
+                message += e.Message;
+                message += "\n" + e.StackTrace;
+                if (e.InnerException != null) 
+                {
+                    message += "\n" + e.InnerException.Message;
+                }
+            }
+            Error(message);
+        }
+
+        public static void Info(string info) 
+        {
+            if (Output != null) 
+            {
+                if (Output.InvokeRequired)
+                {
+                    Output.BeginInvoke((MethodInvoker)delegate()
+                    {
+                        Output.AppendText(info);
+                        if (!info.EndsWith("\n")) Output.AppendText("\n");
+                    });
+                }
+                else
+                {
+                    Output.AppendText(info);
+                    if (!info.EndsWith("\n")) Output.AppendText("\n");
+                }
+            }
+            //if (OutputPane != null) 
+            //{            
+            //    OutputPane.OutputString(info);
+            //    if (!info.EndsWith("\n")) OutputPane.OutputString("\n");
+            //}
+        }
+    }
+}
